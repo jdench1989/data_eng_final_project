86039a4b6a51c29939b441cf20d347311e1e8a0d
Gina Medina <ga.medina0613@gmail.com>
2024-01-08 21:27:02 +0000

    test2

--- airflow/DAGS_PROD/alb_lhpw.py
+++ airflow/DAGS_PROD/alb_lhpw.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'airflow',
