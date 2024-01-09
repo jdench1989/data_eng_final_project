f8f7b9c09d9f4564c5ca0262ff25cbfbc54b2aba
Niki Stefanopoulou <nikistefanopoulou@nikis-air.home>
2024-01-09 16:39:46 +0000

    DAG changed

--- airflow/DAGS_PROD/alb_lhpw.py
+++ airflow/DAGS_PROD/alb_lhpw.py
@@ -14,56 +14,94 @@ default_args = {
     'retry_delay': timedelta(minutes=2),
 }
 
-dag = DAG(
-    'sql_dag',
-    default_args=default_args,
-    description='take all the data',
-    schedule_interval=timedelta(minutes=2),
-    start_date=datetime(2020, 4, 28),
-    catchup=False,
-)
+# dag = DAG(
+#     'sql_dag',
+#     default_args=default_args,
+#     description='take all the data',
+#     schedule_interval=timedelta(minutes=2),
+#     start_date=datetime(2020, 4, 28),
+#     catchup=False,
+# )
 
 ###### alb - ALBANIA #####
-choose_data = """
-SELECT count(*)
-FROM test;
-"""
+# choose_data = """
+# SELECT count(*)
+# FROM test;
+# """
 
-def choosing_data(ti):
-    pg_hook = PostgresHook.get_hook('analytical_db_connection')
-    results = pg_hook.get_records(choose_data)
-    if results[0][0] is None:
-        ti.xcom_push(key='offset', value='0')
-    else:
-        ti.xcom_push(key='offset', value=results[0])
+# def choosing_data(ti):
+#     pg_hook = PostgresHook.get_hook('analytical_db_connection')
+#     results = pg_hook.get_records(choose_data)
+#     if results[0][0] is None:
+#         ti.xcom_push(key='offset', value='0')
+#     else:
+#         ti.xcom_push(key='offset', value=results[0])
     
-t1 = PythonOperator(task_id="task1", python_callable = choosing_data, dag = dag )
+# t1 = PythonOperator(task_id="task1", python_callable = choosing_data, dag = dag)
 
-extract_data_alb_sql = """
-SELECT cnt, TMINS, escs, pared, hisei, homepos, durecec, belong
-    FROM responses
-    OFFSET %s
-    LIMIT 1000;
+# extract_data_alb_sql = """
+# SELECT id, cnt, TMINS, escs, pared, hisei, homepos, durecec, belong
+#     FROM responses;
+# """
+
+#def extract_data_alb(ti):
+    #imported = ti.xcom_pull(key = 'offset')
+    # pg_hook = PostgresHook.get_hook('alb_source_db_connection_to_airflow')
+    # results = pg_hook.get_records(extract_data_alb_sql)
+    # ti.xcom_push(key='results', value=results) # comunica la tarea 3 con las otras tareas
+
+#t1 = PythonOperator(task_id="task1", python_callable = extract_data_alb, dag = dag )
+
+#def insert_data(ti):
+    # imported = ti.xcom_pull(key='results')
+    # pg_hook = PostgresHook.get_hook('analytical_db_connection')
+    # target_fields = ['cnt', 'TMINS', 'escs', 'pared', 'hisei', 'homepos', 'durecec', 'belong']
+    # pg_hook.insert_rows('test', imported, target_fields)
+
+#t2 = PythonOperator(task_id="task2", python_callable = insert_data, dag = dag )
+
+# t1 >>
+#t1 >> t2 
+def extract_data():
+    source_hook = PostgresHook(postgres_conn_id='alb_source_db_connection_to_airflow')
+
+    extract_data_sql = """
+    SELECT id, cnt, tmins, escs, pared, hisei, homepos, durecec, belong
+    FROM responses;
 """
+    extracted_data = source_hook.get_records(extract_data_sql)
+    return extracted_data
 
-def extract_data_alb(ti):
-    imported = ti.xcom_pull(key = 'offset')
-    pg_hook = PostgresHook.get_hook('alb_source_db_connection_to_airflow')
-    results = pg_hook.get_records(extract_data_alb_sql, parameters = [imported])
-    ti.xcom_push(key='results', value=results) # comunica la tarea 3 con las otras tareas
+def load_data(**kwargs):
+    destination_hook = PostgresHook(postgres_conn_id='analytical_db_connection')
+    extracted_data = kwargs['task_instance'].xcom_pull(task_ids='extract_data')
+    
+    if extracted_data:
+        for row in extracted_data:
+            row = list(row)
+         
+            insert_query = "INSERT INTO test (submission_id, cnt, tmins, escs, pared, hisei, homepos, durecec, belong) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (submission_id) DO NOTHING"
+            destination_hook.run(insert_query, parameters=row)
+            
+with DAG('postgres_data_transfer', 
+        default_args=default_args, 
+        schedule_interval=timedelta(minutes=7),
+        catchup=False
+        ) as dag:
 
-t2 = PythonOperator(task_id="task2", python_callable = extract_data_alb, dag = dag )
+    extract_data_task = PythonOperator(
+        task_id='extract_data',
+        python_callable=extract_data,
+        provide_context=True,
+    )
 
-def insert_data(ti):
-    imported = ti.xcom_pull(key='results')
-    pg_hook = PostgresHook.get_hook('analytical_db_connection')
-    target_fields = ['cnt', 'TMINS', 'escs', 'pared', 'hisei', 'homepos', 'durecec', 'belong']
-    pg_hook.insert_rows('test', imported, target_fields)
+    load_data_task = PythonOperator(
+        task_id='load_data',
+        python_callable=load_data,
+        provide_context=True,
+    )
 
-t3 = PythonOperator(task_id="task3", python_callable = insert_data, dag = dag )
-
-t1 >> t2 >> t3 
-
+    extract_data_task >> load_data_task
 
 # from datetime import datetime, timedelta
 
