5bc992f4e448e2847ff3a500a21bf8268652e076
Gina Medina <ga.medina0613@gmail.com>
2024-01-09 15:43:58 +0000

    cleanup

--- airflow/DAGS_PROD/alb_lhpw.py
+++ airflow/DAGS_PROD/alb_lhpw.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
