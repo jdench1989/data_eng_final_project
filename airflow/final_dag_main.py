d8b129e6f6173ca9a4d986df145ee63c3b19bee1
jackdench <jdench1989@gmail.com>
2024-01-11 17:35:06 +0000

    changes

--- airflow/final_dag_main.py
+++ airflow/final_dag_main.py
@@ -36,7 +36,7 @@ dag = DAG(
     'etl_dag',
     default_args=default_args,
     description='Cycle through RDS databases for data extraction',
-    schedule_interval= '@hourly',  # Define your preferred schedule
+    schedule_interval= timedelta(minutes=1),  # Define your preferred schedule
     max_active_runs= 1,
     catchup=False  # Decide if you want to backfill or not
 )
