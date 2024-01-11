f12d1b9a61998a1239c9488a8ae012cb9590aa88
jackdench <jdench1989@gmail.com>
2024-01-11 20:36:54 +0000

    changes

--- airflow/final_dag_main.py
+++ airflow/final_dag_main.py
@@ -7,21 +7,29 @@ from airflow.models import Variable
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import Variable, XCom
 
+source_db_country_list = ['alb', 'arg', 'aus', 'aut', 'bel', 'bgr', 'bih', 'blr', 'bra', 'brn', 'can', 'che', 'chl', 'col', 'cri', 'cze', 'deu', 'dnk', 'dom', 'esp']
 
-# Define the function to extract and load data
-def extract_and_load_data(source_conn_id, destination_conn_id, country):
-    source_hook = PostgresHook(postgres_conn_id=source_conn_id)
+def extract_and_load_all_data(**kwargs):
+    destination_conn_id = 'analytical_db_connection'  # Replace with your destination connection ID
     destination_hook = PostgresHook(postgres_conn_id=destination_conn_id)
-    country_offset = int(Variable.get(f'test_extract_offset_{country}', default_var=0))
-    extract_data_sql = f"""SELECT id, cnt, tmins, escs, durecec, belong FROM responses OFFSET {country_offset} LIMIT 1000;"""
-    extracted_data = source_hook.get_records(extract_data_sql)
-    if extracted_data:
-        destination_hook.insert_rows(table="live", rows=extracted_data, target_fields=["submission_id", "cnt", "tmins", "escs", "durecec", "belong"])
-    live_count_sql = f"SELECT COUNT(*) from live WHERE cnt = '{country.upper()}';"
-    live_count = int(destination_hook.get_records(live_count_sql)[0][0])
-    if live_count > country_offset:
-        new_offset = country_offset + live_count
-        Variable.set(f'test_extract_offset_{country}', new_offset)
+
+    for country in source_db_country_list:
+        source_conn_id = f'rds_source_db_{country}'  # Replace with your connection IDs
+        country_offset = int(Variable.get(f'test_extract_offset_{country}', default_var=0))
+        extract_data_sql = f"""SELECT id, cnt, tmins, escs, durecec, belong FROM responses OFFSET {country_offset} LIMIT 1000;"""
+
+        source_hook = PostgresHook(postgres_conn_id=source_conn_id)
+        extracted_data = source_hook.get_records(extract_data_sql)
+
+        if extracted_data:
+            destination_hook.insert_rows(table="live", rows=extracted_data, target_fields=["submission_id", "cnt", "tmins", "escs", "durecec", "belong"])
+
+        live_count_sql = f"SELECT COUNT(*) from live WHERE cnt = '{country.upper()}';"
+        live_count = int(destination_hook.get_records(live_count_sql)[0][0])
+        if live_count > country_offset:
+            new_offset = country_offset + live_count
+            Variable.set(f'test_extract_offset_{country}', new_offset)
+
 
 # Define the DAG
 default_args = {
@@ -38,29 +46,19 @@ dag = DAG(
     'etl_dag',
     default_args=default_args,
     description='Cycle through RDS databases for data extraction',
-    schedule_interval= timedelta(minutes=1),  # Define your preferred schedule
-    max_active_runs= 1,
+    schedule_interval=timedelta(minutes=1),  # Define your preferred schedule
+    max_active_runs=1,
     # concurrency=1,
     catchup=False  # Decide if you want to backfill or not
 )
 
-source_db_country_list = ['alb', 'arg', 'aus', 'aut', 'bel', 'bgr', 'bih', 'blr', 'bra', 'brn', 'can', 'che', 'chl', 'col', 'cri', 'cze', 'deu', 'dnk', 'dom', 'esp']
-# ['alb', 'arg', 'aus', 'aut', 'bel', 'bgr', 'bih', 'blr', 'bra', 'brn', 'can', 'che', 'chl', 'col', 'cri', 'cze', 'deu', 'dnk', 'dom', 'esp']
-# Use commented list once we are ready to bring more countries onboard
+# Define the PythonOperator for the entire process
+extract_load_all_task = PythonOperator(
+    task_id='extract_and_load_all_data',
+    python_callable=extract_and_load_all_data,
+    provide_context=True,  # Pass task instance context
+    dag=dag,
+)
 
-# Loop through 20 RDS databases
-for country in source_db_country_list:
-    source_conn_id = f'rds_source_db_{country}'  # Replace with your connection IDs
-    destination_conn_id = 'analytical_db_connection'  # Replace with your destination connection ID
-    task_id = f'extract_and_load_data_{country}'
-    
-    # Define the PythonOperator for each iteration
-    extract_load_task = PythonOperator(
-        task_id=task_id,
-        python_callable=extract_and_load_data,
-        op_kwargs={'source_conn_id': source_conn_id, 'destination_conn_id': destination_conn_id, 'country': country},
-        provide_context=True,  # Pass task instance context
-        dag=dag,
-    )
-
-extract_load_task
+# Set the task dependencies
+extract_load_all_task
